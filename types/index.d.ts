478cfbe1e4511d106862aa47c56669e0954f1658
Shisha <shishatw225@gmail.com>
2021-10-24 05:19:06 +0800

    將 *.d.ts 搬移到 types 目錄

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-import { RestInstarce, RestOptions } from './core/Rest';
+import type { RestInstarce, RestOptions } from './core/Rest';
 
 declare var rest: {
   (urn: string, options?: object): RestInstarce,
