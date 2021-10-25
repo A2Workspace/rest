6c5cde2a69546a427c8dbdf3e93547a5c3900fc3
Vinson Liao <shishatw225@gmail.com>
2021-10-25 18:11:15 +0800

    補上註解

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import Rest from './core/Rest';
-
 import defaults from './defaults';
 
-function rest(urn, options = {}) {
+/**
+ * @typedef {import('./core/Rest.js').Rest} Rest
+ * @typedef {import('./core/Rest.js').RestOptions} RestOptions
+ */
+
+/**
+ * @param {string} urn
+ * @param {RestOptions} options
+ * @returns {Rest}
+ */
+export default function rest(urn, options = {}) {
   return new Rest(urn, {
     ...cloneDefaultConfig(rest.defaults),
     ...fastDeepClone(options),
   });
 }
 
+/**
+ * @var {RestOptions}
+ */
 rest.defaults = defaults;
 
 /**
@@ -33,5 +45,3 @@ function cloneDefaultConfig(source) {
 function fastDeepClone(object) {
   return JSON.parse(JSON.stringify(object));
 }
-
-export default rest;
