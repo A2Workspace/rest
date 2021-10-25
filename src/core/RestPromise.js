2d158f171e997210aa517ea4db366c2e8910bf92
Vinson Liao <shishatw225@gmail.com>
2021-10-25 14:36:52 +0800

    補上 RestPromise 註解

--- src/core/RestPromise.js
+++ src/core/RestPromise.js
@@ -1,23 +1,62 @@
-import { captureAxiosError, captureStatusCode, captureValidationError } from './captures';
+import {
+  captureAxiosError,
+  captureStatusCode,
+  captureValidationError,
+} from './captures';
 
 export default class RestPromise extends Promise {
-  constructor(executor) {
-    super(executor);
-  }
-
+  /**
+   * @param {AxiosErrorHandler} handler
+   * @returns {RestPromise}
+   */
   catchAxiosError(handler) {
     return this.catch(captureAxiosError(handler));
   }
 
+  /**
+   * @param {(number|number[]|string|string[])} code
+   * @param {AxiosErrorHandler} handler
+   * @returns {RestPromise}
+   */
   catchStatusCode(code, handler) {
     return this.catch(captureStatusCode(code, handler));
   }
 
+  /**
+   * @param {(boolean|ValidationErrorHandler)} brief
+   * @param {ValidationErrorHandler} [handler]
+   * @returns {RestPromise}
+   */
   catchValidationError(brief, handler) {
     return this.catch(captureValidationError(brief, handler));
   }
 }
 
+/**
+ * @param {Promise} promise
+ * @returns {RestPromise}
+ */
 RestPromise.wrap = function (promise) {
   return this.all([promise]);
-}
+};
+
+/**
+ * @callback AxiosErrorHandler
+ * @param {AxiosError} error
+ * @returns {*}
+ */
+
+/**
+ * @callback ValidationErrorHandler
+ * @param {(ValidationMessageBag|BriefValidationMessageBag)} error
+ * @returns {*}
+ */
+
+/**
+ * @typedef ValidationErrorResponse
+ * @type {object}
+ * @property {number} status
+ * @property {Object} data
+ * @property {string} data.message
+ * @property {Object.<string, string[]>} data.errors
+ */
