18e27abbf1efaa871d82d7c30d29c8a50ee6a1da
Vinson Liao <shishatw225@gmail.com>
2021-10-25 14:29:34 +0800

    完成 RestPromise 單元測試

--- test/core/RestPromise.test.js
+++ test/core/RestPromise.test.js
@@ -36,15 +36,99 @@ describe('RestPromise', () => {
     expect(target).toBeInstanceOf(RestPromise);
   });
 
-  describe('axios', () => {
+  describe('真槍實彈', () => {
     afterEach(() => {
       mock.reset();
     });
 
-    test('基本測試', async () => {
-      mock.onGet('/endpoint').reply(200);
+    test('RestPromise.catchAxiosError()', async () => {
+      mock.onPost('/api/endpoint').reply(400);
 
-      const target = RestPromise.wrap(axios.get('/endpoint'));
+      const handleResponse = jest.fn((res) => res.data);
+      const handleAxiosError = jest.fn((error) => error.isAxiosError);
+      const handleError = jest.fn((error) => error);
+
+      await RestPromise.wrap(axios.post('/api/endpoint'))
+        .then(handleResponse)
+        .catchAxiosError(handleAxiosError)
+        .catch(handleError);
+
+      expect(handleResponse).not.toHaveBeenCalled();
+      expect(handleError).not.toHaveBeenCalled();
+
+      expect(handleAxiosError).toHaveBeenCalledTimes(1);
+      expect(handleAxiosError).toBeCalledWith(
+        expect.objectContaining({
+          response: expect.objectContaining({
+            status: expect.any(Number),
+          }),
+        })
+      );
+
+      expect(handleAxiosError).toHaveReturnedWith(true);
     });
+
+    test('RestPromise.catchStatusCode()', async () => {
+      mock.onPost('/api/endpoint').reply(403);
+
+      const handleResponse = jest.fn((res) => res.data);
+      const handleUnauthorized = jest.fn((error) => error.response.status);
+      const handleForbidden = jest.fn((error) => error.response.status);
+      const handleError = jest.fn((error) => error);
+
+      await RestPromise.wrap(axios.post('/api/endpoint'))
+        .then(handleResponse)
+        .catchStatusCode(401, handleUnauthorized)
+        .catchStatusCode(403, handleForbidden)
+        .catch(handleError);
+
+      expect(handleResponse).not.toHaveBeenCalled();
+      expect(handleUnauthorized).not.toHaveBeenCalled();
+      expect(handleError).not.toHaveBeenCalled();
+
+      expect(handleForbidden).toHaveBeenCalledTimes(1);
+      expect(handleForbidden).toBeCalledWith(
+        expect.objectContaining({
+          response: expect.objectContaining({
+            status: expect.any(Number),
+          }),
+        })
+      );
+    });
+
+    test('RestPromise.catchValidationError()', async () => {
+      mock.onPost('/api/endpoint').reply(422, {
+        message: '缺少必要資料',
+        errors: {
+          title: '標題不能為空',
+          name: '名稱不能為空',
+        },
+      });
+
+      const handleResponse = jest.fn((res) => res.data);
+      const handleValidationError = jest.fn((errorBag) => errorBag.first());
+      const handleError = jest.fn((error) => error);
+
+      await RestPromise.wrap(axios.post('/api/endpoint'))
+        .then(handleResponse)
+        .catchValidationError(handleValidationError)
+        .catch(handleError);
+
+      expect(handleResponse).not.toHaveBeenCalled();
+      expect(handleError).not.toHaveBeenCalled();
+
+      expect(handleValidationError).toHaveBeenCalledTimes(1);
+      expect(handleValidationError).toBeCalledWith(
+        expect.objectContaining({
+          response: expect.any(Object),
+          errors: expect.any(Object),
+          message: expect.any(String),
+          first: expect.any(Function),
+        })
+      );
+
+      expect(handleValidationError).toHaveReturnedWith('標題不能為空');
+    });
+
   });
 });
